3df99702acb5bdeaf9ce1124804c9fb9a35baec0
katpavan <45500874+katpavan@users.noreply.github.com>
2023-01-05 02:02:32 +0000

    push up code

---
+++ list-example.py
@@ -0,0 +1,15 @@
+l = [1,2,-8]
+print(l) # [1, 2, -8]
+print(l[0]) # 0 
+print(l[2]) # -8
+print(len(l)) # 3
+
+# print out the last element of the list but don't use the 2
+x = len(l) - 1 # this gets you the last index in the list
+print(l[x]) # -8
+
+
+
+
+
+
